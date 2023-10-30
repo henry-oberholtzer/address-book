c8be695f0dfe3a1a0bc1398b8f80bd57b5278fa1
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-10-30 09:15:12 -0700

    address book js function complete
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com> Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+// Business Logic for AddressBook---
+function AddressBook() {
+    this.contacts = {};
+    this.currentId = 0;
+}
+AddressBook.prototype.addContact = function(contact) {
+    contact.id = this.assignId();
+    this.contacts[contact.id] = contact;
+};
+AddressBook.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+};
+AddressBook.prototype.findContact = function(id) {
+    if (this.contacts[id] !== undefined) {
+        return this.contacts[id];
+    }
+    return false;
+};
+
+AddressBook.prototype.deleteContact = function(id) {
+    if (this.contacts[id] === undefined) {
+        return false;
+    }
+    delete this.contacts[id];
+    return true;
+}
+
+// Business Logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
+};
+
+
+// let addressBook = new AddressBook();
+// let contact = new Contact("Ada", "Lovelace", "503-555-0100");
+// let contact2 = new Contact("Grace", "Hopper", "503-555-0199");
+// addressBook.addContact(contact);
+// addressBook.addContact(contact2);
+// addressBook.findContact(2);
+// > Object.keys(addressBook.contacts)[0];
+// "1"
+// > typeof Object.keys(addressBook.contacts)[0];
+// "string"
+
